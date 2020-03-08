0d913603a4f791e9fa25117d30b32a195aacae10
毛剑 <981587501@qq.com>
2020-03-09 00:25:01 +0800

    2020年3月8日

---
+++ 2020年3月8日/2020年3月8日.py
@@ -0,0 +1,51 @@
+print("hello  world")
+
+print(2 + 2)
+
+num1 = 2
+num2 = 4
+print(num1 * num2)
+
+name = "Mj"
+print(name)
+
+name1, name2, name3 = "Mj", "Jm", "MJJM"
+print(name1, name2, name3)
+
+s = "123456"
+print(s[1])
+print(s[1:5])
+print(s * 2)
+print(s + "789")
+print(s[1:5:2])
+
+list = ['runoob', 786, 2.23, 'john', 70.2]
+print(list)
+print(list[1])
+print(list[1:3])
+print(list[2:])
+
+tuple = ('runoob', 786, 2.23, 'john', 70.2)
+print(tuple)
+print(tuple[1])
+print(tuple[1:3])
+print(tuple[2:])
+
+dict = {}
+dict['one'] = "This is one"
+dict[2] = "This is two"
+print(dict['one'])
+print(dict[2])
+print(dict.keys())
+print(dict.values())
+
+a = 10
+if a > 5:
+    print("ok")
+else:
+    print("haha")
+
+a = 1
+while a < 10:
+    print(a)
+    a += 1
