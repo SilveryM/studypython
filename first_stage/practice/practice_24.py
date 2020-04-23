6c345e543a6f6ba051857550f8146687c9fd6c69
= <=>
2020-04-23 19:23:29 +0800

    practice

--- first_stage/practice/practice_24.py
+++ first_stage/practice/practice_24.py
@@ -1,36 +1,14 @@
 #有一分数序列：2/1，3/2，5/3，8/5，13/8，21/13...求出这个数列的前20项之和。
-'''
-#!/usr/bin/python 
-# -*- coding: UTF-8 -*- 
-a  =  2.0 
-b  =  1.0 
-s  =  0 
-for   n   in   range ( 1 , 21 ) 
-s  +=  a  /  b 
-t  =  a 
-a  =  a  +  b 
-b  =  t 
-print   s #!/usr/bin/python 
-# -*- coding: UTF-8 -*- 
-a  =  2.0 
-b  =  1.0 
-s  =  0.0 
-for   n   in   range ( 1 , 21 ) 
-s  +=  a  /  b 
-b , a  =  a  ,  a  +  b 
-print   s 
-s  =  0.0 
-for   n   in   range ( 1 , 21 ) 
-s  +=  a  /  b 
-b , a  =  a  ,  a  +  b 
-print   s #!/usr/bin/python 
-# -*- coding: UTF-8 -*- 
-a  =  2.0 
-b  =  1.0 
-l  =  [ ] 
-l . append ( a  /  b ) 
-for   n   in   range ( 1 , 20 ) 
-b , a  =  a , a  +  b 
-l . append ( a  /  b ) 
-print   reduce ( lambda   x , y :  x  +  y , l ) 
-'''
+
+a = [2, 3]
+b = [1, 2]
+
+for i in range(18):
+    a.append(a[-2] + a[-1])
+    b.append(b[-2] + b[-1])
+
+c = 0
+for i in range(len(a)):
+    c = c + a[i] / b[i]
+
+print(c)
