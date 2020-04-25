a6372571390038635142c61da533cdc859ca3f78
= <=>
2020-04-25 12:03:58 +0800

    practice

--- first_stage/practice/practice_25.py
+++ first_stage/practice/practice_25.py
@@ -1,4 +1,15 @@
 #求1+2!+3!+...+20!的和。
-'''
 
-'''
+
+def Factorial(num):
+    final_num = 1
+    for i in range(1, num + 1):
+        final_num = final_num * i
+    return final_num
+
+
+if __name__ == "__main__":
+    num = 0
+    for i in range(1, 21):
+        num = num + Factorial(i)
+    print(num)
