0a4850fc9ef526fc7d080ca392d5de6c57c46a08
= <=>
2020-04-25 12:06:31 +0800

    practice

--- first_stage/practice/practice_26.py
+++ first_stage/practice/practice_26.py
@@ -1,13 +1,13 @@
 #利用递归方法求5!。
-'''
-#!/usr/bin/python 
-# -*- coding: UTF-8 -*- 
-def   fact ( j ) 
-sum  =  0 
-if   j  ==  0 
-sum  =  1 
-else 
-sum  =  j  *  fact ( j  -  1 ) 
-return   sum 
-print   fact ( 5 ) 
-'''
+
+
+def fact(num):
+    sum = 0
+    if (num == 0):
+        sum = 1
+    else:
+        sum = num * fact(num - 1)
+    return sum
+
+
+print(fact(5))
