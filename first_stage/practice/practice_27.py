1f373ba96a5efa4d031a958e47c94148ff1e9c91
SilveryM <981587501@qq.com>
2020-07-13 10:54:56 +0800

    2020年7月13日

--- first_stage/practice/practice_27.py
+++ first_stage/practice/practice_27.py
@@ -1,13 +1,11 @@
 #利用递归函数调用方式，将所输入的5个字符，以相反顺序打印出来。
-'''
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
-def   output ( s , l )
-if   l == 0
-return
-print   ( s [ l - 1 ] )
-output ( s , l - 1 )
-s  =  raw_input ( ' Input a string: ' )
-l  =  len ( s )
-output ( s , l )
-'''
+def output(s, l):
+    if l == 0:
+        return
+    print(s[l - 1], end='')
+    return output(s, l - 1)
+
+if __name__ == "__main__":
+    s = input('input words:')
+    output(s, len(s))
+    print('')
